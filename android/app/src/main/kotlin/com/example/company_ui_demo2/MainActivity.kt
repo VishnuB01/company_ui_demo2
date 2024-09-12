0f11a3f386caa419121f6544a74548600287fe39
VishnuB01 <baasvishnu@gmail.com>
2024-09-12 16:13:24 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/company_ui_demo2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.company_ui_demo2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:company_ui_demo2/HomeScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+
+   home:HomeScreen(),
+   
+    );
+  }
+}
